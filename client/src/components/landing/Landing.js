cac7d1177cf5174db0ace2794ace8ec29775c5de
Zachary Legros <legroszach3921@gmail.com>
2020-02-08 16:58:51 -0500

    change landing links

--- client/src/components/landing/Landing.js
+++ client/src/components/landing/Landing.js
@@ -3,15 +3,27 @@ import Context from "../context/Context";
 
 const Landing = props => {
   const context = useContext(Context);
+  const [auth, setAuth] = useState(false);
 
-  useEffect(() => {
+  useEffect(() => {
+    context.checkIfLoggedIn().then(auth => {
+      setAuth(auth);
+    });
   }, []);
 
   return (
     <React.Fragment>
-       <span>Landing page</span>
-       <a href="/login">Login</a>
-       <a href="/register">Register</a>
+      <span>Landing page</span>
+      {auth ? (
+        <React.Fragment>
+          <a href="/user/dashboard">My account</a>
+        </React.Fragment>
+      ) : (
+        <React.Fragment>
+          <a href="/login">Login</a>
+          <a href="/register">Register</a>
+        </React.Fragment>
+      )}
     </React.Fragment>
   );
 };
